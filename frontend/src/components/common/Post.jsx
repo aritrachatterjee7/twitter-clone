7653bea84dd9dee1a874392bb922243f1efe9cb6
aritrachatterjee7 <161926590+aritrachatterjee7@users.noreply.github.com>
2024-07-17 22:07:44 +0530

    Comment function added

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -10,6 +10,7 @@ import { toast } from "react-hot-toast";
 
 
 import LoadingSpinner from "./LoadingSpinner";
+import { formatPostDate } from "../../utils/date";
 
 const Post = ({ post }) => {
 	const [comment, setComment] = useState("");
@@ -69,14 +70,43 @@ const Post = ({ post }) => {
 		},
 	}); 
 
+	const { mutate: commentPost, isPending: isCommenting } = useMutation({
+		mutationFn: async ({ postId }) => {
+			try {
+				const res = await fetch(`/api/posts/comment/${postId}`, {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({ text: comment }),
+				});
+				const data = await res.json();
+	
+				if (!res.ok) {
+					throw new Error(data.error || "Something went wrong");
+				}
+				return data;
+			} catch (error) {
+				throw new Error(error);
+			}
+		},
+		onSuccess: () => {
+			toast.success("Comment posted successfully");
+			setComment("");
+			queryClient.invalidateQueries({ queryKey: ["posts"] });
+		},
+		onError: (error) => {
+			toast.error(error.message);
+		},
+	});
+	
+
 	const postOwner = post.user;
 	const isLiked = post.likes.includes(authUser._id);
 
 	const isMyPost = authUser._id === post.user._id;
 
-	const formattedDate = "1h";
-
-	const isCommenting = true;
+	const formattedDate = formatPostDate(post.createdAt)
 
 	const handleDeletePost = () => {
 		deletePost()
@@ -84,7 +114,10 @@ const Post = ({ post }) => {
 
 	const handlePostComment = (e) => {
 		e.preventDefault();
+		if (isCommenting || comment.trim() === "") return; // Check if already commenting or comment is empty
+		commentPost({ postId: post._id }); // Pass postId to commentPost mutation function
 	};
+	
 
 	const handleLikePost = () => {
 		if (isLiking) return;
